08bff5dcdc32aca57f9dbda3005ebc0761dc2d1b
Murilo Gomes <antoniomuriloaraujodev@gmail.com>
2024-05-26 23:13:37 -0300

    sem termina

--- script/script.js
+++ script/script.js
@@ -1,37 +1,26 @@
 let imgLogo = document.getElementById("logo");
 imgLogo.setAttribute("src", "./assets/icon-logo-nav.svg");
 let hora = new Date();
-let agora = hora.setHours()
+let agora = hora.getHours
+let mundHorario = agora
 let avatar = document.getElementById("avatar");
 let tituloPricipal = document.getElementById("titulo-principal");
 
 if(agora >= 0 && agora < 6) {
     document.body.style.backgroundColor = "#2d6c89";
+    document.body.style.color = "white";
+    avatar.setAttribute("src", "../img/img1.jpg");
 } else if(agora >= 6 && agora < 12) {
     document.body.style.backgroundColor = "#f9d487";
-} else if (agora >= 12 && agora < 18) {
-    document.body.style.backgroundColor = "#fcbea4";
-} else if(agora >= 18 && agora <= 23) {
-    document.body.style.backgroundColor = "#193d4b";
-}
-
-if(agora >= 0 && agora < 6) {
-    document.body.style.color = "white";
-} else if(agora >= 6 && agora < 12) {
     document.body.style.color = "black";
-} else if (agora >= 12 && agora < 18) {
-    document.body.style.color = "black";
-} else if(agora >= 18 && agora <= 23) {
-    document.body.style.color = "white";
-}
-
-if(agora >= 0 && agora < 6) {
-    avatar.setAttribute("src", "../img/img1.jpg");
-} else if (agora >= 6 && agora < 12) {
     avatar.setAttribute("src", "../img/img2.jpg");
 } else if (agora >= 12 && agora < 18) {
+    document.body.style.backgroundColor = "#fcbea4";
+    document.body.style.color = "black";
     avatar.setAttribute("src", "../img/img3.jpg");
-} else if (agora >= 18 && agora <= 23) {
+} else if(agora >= 18 && agora <= 23) {
+    document.body.style.backgroundColor = "#193d4b";
+    document.body.style.color = "white";
     avatar.setAttribute("src", "../img/img4.jpg");
 }
 
