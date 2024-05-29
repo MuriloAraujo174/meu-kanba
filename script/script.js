9a179e0c86eff3dbd3dc745863291576ac3b463c
Murilo Gomes <antoniomuriloaraujodev@gmail.com>
2024-05-29 14:32:26 -0300

    new update

--- script/script.js
+++ script/script.js
@@ -1,28 +1,31 @@
 let imgLogo = document.getElementById("logo");
 imgLogo.setAttribute("src", "./assets/icon-logo-nav.svg");
-let hora = new Date();
-let agora = hora.getHours
-let mundHorario = agora
+let hora = new Date().getHours();
 let avatar = document.getElementById("avatar");
 let tituloPricipal = document.getElementById("titulo-principal");
 
-if(agora >= 0 && agora < 6) {
-    document.body.style.backgroundColor = "#2d6c89";
-    document.body.style.color = "white";
-    avatar.setAttribute("src", "../img/img1.jpg");
-} else if(agora >= 6 && agora < 12) {
-    document.body.style.backgroundColor = "#f9d487";
-    document.body.style.color = "black";
-    avatar.setAttribute("src", "../img/img2.jpg");
-} else if (agora >= 12 && agora < 18) {
-    document.body.style.backgroundColor = "#fcbea4";
-    document.body.style.color = "black";
-    avatar.setAttribute("src", "../img/img3.jpg");
-} else if(agora >= 18 && agora <= 23) {
-    document.body.style.backgroundColor = "#193d4b";
-    document.body.style.color = "white";
-    avatar.setAttribute("src", "../img/img4.jpg");
+try {
+    if(hora >= 0 && hora < 6) {
+        document.body.style.backgroundColor = "#2d6c89";
+        document.body.style.color = "white";
+        avatar.setAttribute("src", "../img/img1.jpg");
+    } else if(hora >= 6 && hora < 12) {
+        document.body.style.backgroundColor = "#f9d487";
+        document.body.style.color = "black";
+        avatar.setAttribute("src", "../img/img2.jpg");
+    } else if (hora >= 12 && hora < 18) {
+        document.body.style.backgroundColor = "#fcbea4";
+        document.body.style.color = "black";
+        avatar.setAttribute("src", "../img/img3.jpg");
+    } else if(hora >= 18 && hora <= 23) {
+        document.body.style.backgroundColor = "#193d4b";
+        document.body.style.color = "white";
+        avatar.setAttribute("src", "../img/img4.jpg");
+    }    
+} catch (error) {
+    console.log(error)
 }
+
 
 function menuClick() {
     let menuRes = document.getElementById("itens")
